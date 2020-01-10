cb85fd214ab312c552a92b2acccb69af5761f74c
amit-kumar-mahato <amitraj0589@gmail.com>
2020-01-10 19:24:52 +0530

    [Amit] Add . Implemented Redis Cache on note

--- FundooNotes/src/main/java/com/blbz/fundoonotes/serviceimpl/NoteServiceImpl.java
+++ FundooNotes/src/main/java/com/blbz/fundoonotes/serviceimpl/NoteServiceImpl.java
@@ -3,11 +3,13 @@ package com.blbz.fundoonotes.serviceimpl;
 import java.time.LocalDateTime;
 import java.util.List;
 import java.util.Optional;
+import java.util.concurrent.TimeUnit;
 
 import org.modelmapper.ModelMapper;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.redis.core.RedisTemplate;
 import org.springframework.stereotype.Service;
 
 import com.amazonaws.services.apigateway.model.Op;
@@ -47,15 +49,18 @@ public class NoteServiceImpl implements INoteService {
 	
 	@Autowired
 	ElasticSearchService elasticSearchService;
+	
+	@Autowired
+	RedisTemplate<String, Object> redisTemplate;
 
 	@Override
 	public boolean computeSave(NoteDto noteDto, String token) {
 		
-		long id = jwtGenerator.parseJWT(token);
-		//LOGGER.info("Id is :"+id+" ,Description :"+noteDto.getDescription());
-		log.info("Id is :"+id+" ,Description :"+noteDto.getDescription());
+		long userId = getRedisCacheId(token);
+		//long id = jwtGenerator.parseJWT(token);
+		log.info("Id is :"+userId+" ,Description :"+noteDto.getDescription());
 		
-		Optional<User> user = userRepository.findById(id);
+		Optional<User> user = userRepository.findById(userId);
 		if(user.isPresent()) {
 			note = modelMapper.map(noteDto, Note.class);
 			note.setUserNotes(user.get());
@@ -81,7 +86,8 @@ public class NoteServiceImpl implements INoteService {
 
 	@Override
 	public boolean deleteOneNote(long id, String token) throws NoteIdNotFoundException {
-		long userId = jwtGenerator.parseJWT(token);
+		//long userId = jwtGenerator.parseJWT(token);
+		long userId = getRedisCacheId(token);
 	Optional<User> isUserAvailable = userRepository.findById(userId);
 	if(isUserAvailable.isPresent()) {
 		Optional<Note> isNoteIdAvailable = noteRepository.findById(id);
@@ -98,7 +104,8 @@ public class NoteServiceImpl implements INoteService {
 
 	@Override
 	public boolean isArchived(long id, String token){
-		long userId = jwtGenerator.parseJWT(token);
+		//long userId = jwtGenerator.parseJWT(token);
+		long userId = getRedisCacheId(token);
 		Optional<User> user = userRepository.findById(userId);
 		if(user.isPresent()) {
 			Optional<Note> isNoteAvailable = noteRepository.findById(id);
@@ -114,11 +121,12 @@ public class NoteServiceImpl implements INoteService {
 
 	@Override
 	public List<Note> getAllNotes(String token) {
-		long id = jwtGenerator.parseJWT(token);
-		Optional<User> isUserAvailable = userRepository.findById(id);
+		//long id = jwtGenerator.parseJWT(token);
+		long userId = getRedisCacheId(token);
+		Optional<User> isUserAvailable = userRepository.findById(userId);
 		List<Note> getAllNotes = null;
 		if(isUserAvailable.isPresent()) {
-			getAllNotes = noteRepository.getAllNotes(id);
+			getAllNotes = noteRepository.getAllNotes(userId);
 		}
 		return getAllNotes;
 	}
@@ -126,7 +134,8 @@ public class NoteServiceImpl implements INoteService {
 	@Override
 	public boolean addColor(String color, String token, long id) {
 		
-		long userId = jwtGenerator.parseJWT(token);
+		//long userId = jwtGenerator.parseJWT(token);
+		long userId = getRedisCacheId(token);
 		Optional<User> isUserAvailable = userRepository.findById(userId);
 		if(isUserAvailable.isPresent()) {
 			Optional<Note> isNoteAvailable = noteRepository.findById(id);
@@ -141,7 +150,8 @@ public class NoteServiceImpl implements INoteService {
 
 	@Override
 	public boolean pinnedNotes(long id, String token) {
-		long userId = jwtGenerator.parseJWT(token);
+		//long userId = jwtGenerator.parseJWT(token);
+		long userId = getRedisCacheId(token);
 		Optional<User> isUserAvailable = userRepository.findById(userId);
 		if(isUserAvailable.isPresent()) {
 			Optional<Note> isNoteAvailable = noteRepository.findById(id);
@@ -157,7 +167,8 @@ public class NoteServiceImpl implements INoteService {
 
 	@Override
 	public boolean setReminder(long noteId, String token,ReminderDto reminderDto) {
-		long userId = jwtGenerator.parseJWT(token);
+		//long userId = jwtGenerator.parseJWT(token);
+		long userId = getRedisCacheId(token);
 		Optional<User> isUserAvailable = userRepository.findById(userId);
 		if(isUserAvailable.isPresent()) {
 			Optional<Note> isNoteAvailable = noteRepository.findById(noteId);
@@ -170,7 +181,8 @@ public class NoteServiceImpl implements INoteService {
 
 	@Override
 	public boolean permanentDelete(long noteId, String token) {
-		long userId = jwtGenerator.parseJWT(token);
+		//long userId = jwtGenerator.parseJWT(token);
+		long userId = getRedisCacheId(token);
 		Optional<User> isUserAvailable = userRepository.findById(userId);
 		if(isUserAvailable.isPresent()) {
 			Optional<Note> isNoteAvailable = noteRepository.findById(noteId);
@@ -186,5 +198,20 @@ public class NoteServiceImpl implements INoteService {
 	public List<Note> searchByTitle(String title) {
 		List<Note> notes = elasticSearchService.searchByTitle(title);
 		return notes;
+	}
+	
+	/*
+	 * Add data to the redis cache
+	 * */
+	private long getRedisCacheId(String token) {
+		String[] splitedToken = token.split("\\.");
+		String redisTokenKey = splitedToken[1] + splitedToken[2];
+		if (redisTemplate.opsForValue().get(redisTokenKey) == null) {
+			long idForRedis = jwtGenerator.parseJWT(token);
+			log.info("idForRedis is :" + idForRedis);
+			redisTemplate.opsForValue().set(redisTokenKey, idForRedis, 3 * 60, TimeUnit.SECONDS);
+		}
+		long userId = (Long) redisTemplate.opsForValue().get(redisTokenKey);
+		return userId;
 	}
 }
