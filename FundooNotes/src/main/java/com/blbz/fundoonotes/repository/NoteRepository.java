9899f2ea25394d49a555d71316416451f40efc20
amit-kumar-mahato <amitraj0589@gmail.com>
2020-02-05 09:54:37 +0530

    [Amit] Update Query

--- FundooNotes/src/main/java/com/blbz/fundoonotes/repository/NoteRepository.java
+++ FundooNotes/src/main/java/com/blbz/fundoonotes/repository/NoteRepository.java
@@ -8,16 +8,20 @@ import org.springframework.data.jpa.repository.Modifying;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.CrudRepository;
 
+import com.blbz.fundoonotes.model.Label;
 import com.blbz.fundoonotes.model.Note;
 
 @Transactional
 public interface NoteRepository extends CrudRepository<Note, Long> {
 
-	@Query(value = "SELECT * FROM note where user_id=? AND is_trash=false", nativeQuery = true)
+	@Query(value = "SELECT * FROM note where user_id=? AND is_trash=false AND is_archiev=false", nativeQuery = true)
 	List<Note> getAllNotes(long id);
 
 	@Modifying
 	@Query(value = "DELETE FROM note where note_id=? AND is_trash=true", nativeQuery = true)
 	void deleteNotesPermanently(long noteId);
+
+	@Query(name="tofindlabelfornote", nativeQuery = true)
+	List<Label> getLabelsByNoteId(long noteId);
 
 }
